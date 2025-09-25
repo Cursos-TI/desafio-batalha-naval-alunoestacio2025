f322fec52a78f69bb55eba05dde5141c245098e7
aluno2025 <202507053541@alunos.estacio.br>
2025-09-25 00:20:27 +0000

    Nível Avançado + correção da ordem do codigo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,59 +4,78 @@
 #define COLUNAS 10
 
 int main() {
-
-
-    // Letras das colunas (horizontal)
-    char letras[LINHAS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-
-    // Números das linhas (vertical)
-    int numeros[COLUNAS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    char letras[COLUNAS] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int numeros[LINHAS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int tabuleiro[LINHAS][COLUNAS] = {0};
 
-   /*
-    tabuleiro[6][6] = 3;
-    tabuleiro[7][6] = 3;
-    tabuleiro[8][6] = 3; */
+    // ADICIONANDO NAVIOS E HABILIDADES FORA DO LOOP DE IMPRESSÃO
 
-    // Título do tabuleiro
+    // HORIZONTAL:
+    int linha = 0, coluna = 0;
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha][coluna + i] = 3;
+    }
+
+    // VERTICAL:
+    linha = 0; coluna = 9;
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha + i][coluna] = 3;
+    }
+
+    // DIAGONAL:
+    linha = 0; coluna = 7;
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[linha + i][coluna - i] = 3;
+    }
+
+    // CONE:
+    int linha_inicio = 7;
+    int linha_centro = 2;
+    for (int i = 0; i < 3; i++) {
+        for (int j = -i; j <= i; j++) {
+            tabuleiro[linha_inicio + i][linha_centro + j] = 3;
+        }
+    }
+
+    // CRUZ:
+    int linha_centro_cruz = 7;
+    int coluna_centro_cruz = 7;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -2; j <= 2; j++) {
+            if (i == 0 || j == 0) {
+                tabuleiro[linha_centro_cruz + i][coluna_centro_cruz + j] = 3;
+            }
+        }
+    }
+
+    // OCTAEDRO:
+    int linha_centro_octaedro = 3;
+    int coluna_centro_octaedro = 2;
+
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (i == 0 || j == 0) {
+            tabuleiro[linha_centro_octaedro + i][coluna_centro_octaedro + j] = 3;
+        }
+    }
+}
+
+
+    // IMPRIME O TABULEIRO
     printf("   TABULEIRO BATALHA NAVAL\n   ");
-
-    // Imprime letras das linhas
-    for (int i = 0; i < LINHAS; i++) {
+    for (int i = 0; i < COLUNAS; i++) {
         printf("%c ", letras[i]);
     }
     printf("\n");
 
-    //##### LOOP ALINHADO - ÍNICIO #####
-
-    // Imprime números + interior do tabuleiro
-    for (int i = 0; i < COLUNAS; i++) {
+    for (int i = 0; i < LINHAS; i++) {
         printf("%2d ", numeros[i]);
-        for (int j = 0; j < LINHAS; j++) {
+        for (int j = 0; j < COLUNAS; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
-    // ADICONANDO NAVIO
-    // HORIZONTAL
-     int coluna = 2, linha = 1;
-     for (int i = 0; i < 3; i++) {
-     tabuleiro[coluna][linha + i] = 3;
-    }
-    // VERTICAL
-     coluna = 6, linha = 6;
-     for (int i = 0; i < 3; i++){
-     tabuleiro[coluna + i][linha] = 3;
-    }
-    // DIAGONAL
-     coluna = 1, linha = 9;
-     for (int i = 0; i < 3; i++){
-     tabuleiro[coluna + i][linha - i] = 3;
-    }
-
-
         printf("\n");
     }
-
 
     return 0;
 }
